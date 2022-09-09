a063d60e21aef70a3e541ecfa8c38b804aa1cd20
Carlos <50680051+carlosrvilela@users.noreply.github.com>
2022-09-09 19:31:01 -0300

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["os"], "excludes": []}
+build_exe_options = {"packages": ["os"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
